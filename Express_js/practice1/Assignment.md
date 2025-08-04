6c48c9f8edc31db98b0e469a804e299476505936
codermeghagithub <meghade2003@gmail.com>
2025-08-04 18:21:36 +0530

    Implement PATCH and DELETE

--- Express_js/practice1/Assignment.md
+++ Express_js/practice1/Assignment.md
@@ -28,3 +28,55 @@ It can handle a large number of concurrent connections due to Node.js's event-dr
 
 
 ## How to create endpoints for each method
+
+
+
+##  explain patch method 
+```
+app.patch("/get-user/:id", (req, res) => {
+  const id = parseInt(req.params.id); // Get user ID from URL
+  const updates = req.body;           // Get the fields to update from request body
+
+  // Find the user by ID
+  const userIndex = data.findIndex(u=>u.id===id);
+
+// or for understand   const userIndex = data.findIndex(u=>u.id===id); syntax ✅✅
+//  const userIndex = data.findIndex(function(u) {
+//   return u.id === id;
+// });
+
+
+  if (userIndex === -1) {
+    return res.status(404).json({ success: false, message: "User not found" });
+  }
+
+
+  // Update only provided fields
+  data[userIndex] = { ...data[userIndex], ...updates };
+// ...data[userIndex] means :all key-value pairs of the user object at that index.   
+
+// Becomes:
+
+// data[1] = {  // full menaing of ...data[userIndex] 
+//   id: 2,
+//   name: "Sneha",
+//   email: "newemail@example.com"  // This **overwrites** the old email
+// };
+
+// 🔹 ...updates
+// This means:
+
+// "Take all key-value pairs from the updates object and add them too."
+
+  res.json({
+    success: true,
+    message: "User updated successfully",
+    user: data[userIndex]
+  });
+});  //👉🏻Sends a JSON response with:
+
+success: true
+A success message
+The updated user object
+
+```
--- Express_js/practice1/put.js
+++ Express_js/practice1/put.js
@@ -87,6 +87,72 @@
 // PATCH
 
 
+// import express from "express";
+// const app=express()
+
+// app.use(express.json());
+
+// const data=[
+// {
+// id:1,
+// name:"Megha",
+// age:22,
+// email:"megha123@gmail.com"
+// },
+// {
+// id:2,
+// name:"Sneha",
+// email:"sneha123@gmail.com"
+// },
+// {
+// id:3,
+// name:"Meg",
+// email:"meg123@gmail.com"
+// },
+// ];
+
+// app.patch("/get-user/:id", (req, res) => {
+//   const id = parseInt(req.params.id); // Get user ID from URL
+//   const updates = req.body;           // Get the fields to update from request body
+
+//   // Find the user by ID
+//   const userIndex = data.findIndex(u=>u.id===id);
+
+// // or for understand   const userIndex = data.findIndex(u=>u.id===id); syntax ✅✅
+// //  const userIndex = data.findIndex(function(u) {
+// //   return u.id === id;
+// // });
+
+
+//   if (userIndex === -1) {
+//     return res.status(404).json({ success: false, message: "User not found" });
+//   }
+
+
+//   // Update only provided fields
+//   data[userIndex] = { ...data[userIndex], ...updates };
+// // ...data[userIndex] means :all key-value pairs of the user object at that index.   
+
+
+
+//   res.json({
+//     success: true,
+//     message: "User updated successfully",
+//     user: data[userIndex]
+//   });
+// });
+
+// //here we createing server for express js
+// app.listen(8080,()=>{
+//   console.log("Hello everyone server is running on 8080");
+
+// }
+// )
+
+
+// DELETE
+
+
 import express from "express";
 const app=express()
 
@@ -111,32 +177,23 @@ email:"meg123@gmail.com"
 },
 ];
 
-app.patch("/get-user/:id", (req, res) => {
-  const id = parseInt(req.params.id); // Get user ID from URL
-  const updates = req.body;           // Get the fields to update from request body
+app.delete("/get-user/:id",(req,res)=>{
+  const id=parseInt(req.params.id);
+  const userIndex=data.findIndex(u=>u.id==id);
 
-  // Find the user by ID
-  const userIndex = data.findIndex(u => u.id === id);
-  if (userIndex === -1) {
-    return res.status(404).json({ success: false, message: "User not found" });
+  if(userIndex===-1){
+    return res.status(404).json({success:false,message:"user not found "})
   }
 
-  // Update only provided fields
-  data[userIndex] = { ...data[userIndex], ...updates };
+const delete_user=data.splice(userIndex,1);
 
-  res.json({
-    success: true,
-    message: "User updated successfully",
-    user: data[userIndex]
-  });
+res.json({
+  success: true,
+    message: "User deleted successfully",
+    delete_user: delete_user[0] // sending deleted user info
+});
 });
 
-//here we createing server for express js
-app.listen(8080,()=>{
+ app.listen(8080,()=>{
   console.log("Hello everyone server is running on 8080");
-
-}
-)
-
-
-// DELETE
+ })
