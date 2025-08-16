fd5822c354f1f70d5883dcd00257cc918a58fa78
codermeghagithub <meghade2003@gmail.com>
2025-08-16 17:32:38 +0530

    log middleware done

--- Express_js/Mini_project/routers/public.routes.js
+++ Express_js/Mini_project/routers/public.routes.js
@@ -1,20 +1,20 @@
-import {Router} from "express";
-const router=Router();
+import { Router } from "express";
+const router = Router();
 
-
-// here we create generate token endpoint 
-router.get("/generate-token",(req,res)=>{
-  // todo: calling utils method to create a random token 
-  const token ="token";
+// here we create generate token endpoint
+router.get("/generate-token", (req, res) => {
+  // todo: calling utils method to create a random token
+  const token = "token";
   res.status(200).json({
-    message:"Token generated please save it for the future use ",
-    data:token
-  })
-})
+    message: "Token generated please save it for the future use ",
+    data: token,
+  });
+});
 
 // home route
-router.get("/",(req,res)=>{
-  res.status(200).json({message:"Welcome to home page" })
-})
+router.get("/", (req, res) => {
+  // * this is anoter endpoint
+  res.status(200).json({ message: "Welcome to home page" });
+});
 
 export default router;
