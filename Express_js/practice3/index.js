45c8dae9c7eaba524829b8291e206cc2c37486b0
codermeghagithub <meghade2003@gmail.com>
2025-08-27 20:58:32 +0530

    feat: add cookie-parser and cookie-signature modules
    - Implemented cookie-parser to parse HTTP request cookies and populate req.cookies and req.signedCookies.
    - Added cookie-signature for signing and unsigning cookies.
    - Included necessary dependencies and updated package.json files for both modules.
    - Added documentation files including README, LICENSE, and SECURITY for cookie and cookie-signature modules.
    - Established a clear structure for cookie parsing and serialization in the cookie module.

--- Express_js/practice3/index.js
+++ Express_js/practice3/index.js
@@ -1,21 +1,27 @@
-import express from "express"
-const app=express();
-app.get("/",(req,res)=>{
+import express from "express";
+import cookieParser from "cookie-parser";
+
+// const cookieParser=cookieParser();
+const app = express();
+app.use(cookieParser()); //middleware
+
+app.get("/", (req, res) => {
   res.send("Hello world");
 });
 
-app.get('/set-cookies',(req,res)=>{
-  res.cookie("token","my-token");
-  res.send("cookies set")
-})
-app.get("/get-cookies",(req,res)=>{
-  // console.log(req.cookies);//**undefined */
-   // **if i acess cookie through token  that time i get out put  token=my-token
-    console.log(req.headers.cookie);  
-  res.send("cookies get") 
-  
+app.get("/set-cookies", (req, res) => {
+  // res.cookie("token", "my-token", { maxAge: 1000 * 60 * 60 * 24 }); // ** here cookie autometically expire after 24 hr
 
-})
-app.listen(8080,()=>{
-  console.log("server is running on port 8080")
-})
+  res.cookie("theme","dark")
+
+  res.send("cookies set");
+});
+app.get("/get-cookies", (req, res) => {
+  console.log(req.cookies); //**undefined */
+  // **if i acess cookie through token  that time i get out put  token=my-token
+  // console.log(req.headers.cookie);
+  res.send("cookies get");
+});
+app.listen(8080, () => {
+  console.log("server is running on port 8080");
+});
